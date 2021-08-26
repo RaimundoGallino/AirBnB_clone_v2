ffb99976b2862a72a1c1442e9958e0a7d0acbd85
RaimundoGallino <raigallino0@gmail.com>
2021-08-26 15:33:19 -0300

    Uploading tas 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the web_static
+"""
+
+from Fabric import local
+from datetime import datetime
+
+
+def do_pack():
+    """pack the files"""
+    local("mkdir -p versions", capture=False)
+    filename = "web_static_" + datetime.strftime(datetime.now(),
+                                                 "%Y%m%d%H%M%S") + ".tgz"
+    try:
+        ret = local("tar -cvzf" + filename + ".tgz web_static", capture=True)
+        return ret
+    except:
+        return None
