140a9a1dba103a4e14b7beaf07854875f04206bd
RaimundoGallino <raigallino0@gmail.com>
2021-09-11 17:39:28 -0300

    Uploading readme

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def home():
     """starts a Flask web application"""
     return "Hello HBTN!"
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
